baf9914327a3ea4d354a02aa3ba8d53111bf9b69
bhuvaneswaryadav <yadavbhuvaneswar782@gmail.com>
2023-11-10 14:15:20 +0530

    jdjdj

---
+++ Java/identifiers/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String[] args){
+        //good
+        int minutesPerHour =50;
+        //ok ,but not easy to understand m actually
+        int m =60;
+        System.out.println(minutesPerHour);
+        System.out.println(m);
+    }
+}
