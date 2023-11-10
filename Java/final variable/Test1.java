558345505a7a5e5068e09961c47871bda44351f0
bhuvaneswaryadav <yadavbhuvaneswar782@gmail.com>
2023-11-10 13:06:31 +0530

    jjsj

---
+++ Java/final variable/Test1.java
@@ -0,0 +1,8 @@
+public class Test1{
+    public static void main(String[] args){
+        String firstname = "yelugu";
+        String lastname  = "bhuvaneswaryadav";
+        String fullname  = firstname+lastname;
+        System.out.println(fullname);
+    }
+}
