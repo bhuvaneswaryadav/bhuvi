bbad618354485420e0338f286e47ca9ae9e49be8
bhuvaneswaryadav <yadavbhuvaneswar782@gmail.com>
2023-11-29 19:31:44 +0530

    shshh

--- Java/inheristance/Test.java
+++ Java/inheristance/Test.java
@@ -1,22 +1,18 @@
-package pack1;
-public class Test{
-        public static void main(String[] args){
-                   Parent  p1=new Parent();
-                  /*  p1.m1();
-                   p1.m2();
-                   p1.m3(); */
-                   
-                   Child c1=new Child();
-                 /*   c1.m1();
-                   c1.m2();
-                   c1.m3();
-                   c1.m4(); */
+class Test{
+    public static void main(String[] args){
+           
+            System.out.println("GM");
+             String ename1 = "Rahul Gandhi";
+            String ename2=null;
+            System.out.println(ename1.length());
+            System.out.println(ename2.length());
+       
+            
+  
 
-                   Parent c2=new Child();
-                   c2.m1();
-                   c2.m2();
-                   c2.m3();
-                   c2.m4();
-                   //Child p2=new Parent();
+              System.out.println("GE");
         }
-}
+       
+        
+       
+   }
